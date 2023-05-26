8455d0e69a19320439e1ac4e6e40505b35e92387
hoanghai2003 <haibajj2k3@gmail.com>
2023-05-26 14:05:51 +0700

    add

--- client/src/component/common/Home.jsx
+++ client/src/component/common/Home.jsx
@@ -1,6 +1,45 @@
+import { useState, useEffect } from "react";
 import "./Home.css";
 
 function Home() {
+  const [inputValue, setInput] = useState("");
+  const [notes, setNotes] = useState("");
+
+  console.log(notes, "---------");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleClick = () => {
+    fetch("http://localhost:3000/api/v1/user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ Content: inputValue }),
+    });
+    setInput("");
+    renderData();
+  };
+
+  const renderData = () => {
+    fetch("http://localhost:3000/api/v1/user", { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => {
+        setNotes([data.users]);
+      });
+  };
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:3000/api/v1/user/:${id}`, { method: "DELETE" })
+      .then((response) => response.json())
+      .then((data) => {
+        renderData();
+      });
+  };
+
+  useEffect(() => {
+    renderData();
+  }, []);
   return (
     <div className="container">
       <header className="app">
@@ -8,15 +47,26 @@ function Home() {
       </header>
       <div className="content">
         <div className="input-bt">
-          <input type="text" placeholder="Title Take a not " />
+          <textarea
+            type="text"
+            placeholder="Title Take a not "
+            onChange={(e) => handleChange(e)}
+          ></textarea>
           <br />
-          <button>+</button>
+          <button onClick={handleClick}>+</button>
         </div>
 
-        <div className="text-div">
-          <strong>aaaaaaaaaaaaa</strong>
-          <button className="delete-btn"> del</button>
-        </div>
+        {notes.data?.map((users) => (
+          <>
+            <div className="text-div">
+              <strong>{users.user_cmt}</strong>
+              <button className="delete-btn" onClick={handleDelete()}>
+                {" "}
+                del
+              </button>
+            </div>
+          </>
+        ))}
       </div>
     </div>
   );
